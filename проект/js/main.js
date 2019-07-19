a3cdb537e756684a6a2dd3f84b234e33ef517e78
RoadToNowhere2 <50709196+RoadToNowhere2@users.noreply.github.com>
2019-07-19 13:50:18 +0400

    Add files via upload

--- проект/js/main.js
+++ проект/js/main.js
@@ -1,28 +1,31 @@
-$('.menu-btn').on('click', function(e) {
+$(".menu-btn").on("click", function(e) {
   e.preventDefault();
-  $('.menu-nav').toggleClass('menu-nav_active');
+  $(".menu-nav").toggleClass("menu-nav_active");
 });
 
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  responsiveClass:true,
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  responsiveClass: true,
   nav: true,
-  navText: ["<img src='../img/lefe_arrow.png'>", "<img src='../img/right_arrow.png'>"],
+  navText: [
+    "<img src='img/lefe_arrow.png'>",
+    "<img src='img/right_arrow.png'>"
+  ],
   autoplay: true,
   autoplayTimeout: 10000,
   autoplayHoverPause: true,
   navSpeed: 1500,
   autoplaySpeed: 1500,
-  responsive:{
-      0:{
-          items:1,
-          nav: false
-      },
-      600:{
-          items:2
-      },
-      1000:{
-          items:3
-      }
+  responsive: {
+    0: {
+      items: 1,
+      nav: false
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
   }
 });

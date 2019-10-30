109b83f9370dcd2ac1d87b04e0db3799f659a8e8
RoadToNowhere2 <50709196+RoadToNowhere2@users.noreply.github.com>
2019-10-31 00:44:43 +0400

    Add files via upload

---
+++ Трудоустройства/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 4,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    autoplaySpeed: 1500,
+    smartSpeed: 1500,
+    smartSpeed: 1500,
+    dragEndSpeed: 1500,
+    loop: true
+  });
+});

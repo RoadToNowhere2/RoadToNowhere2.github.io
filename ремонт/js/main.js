fb856d5cfd3e6e2ff71c1ce8d53bf054ed3ba13c
RoadToNowhere2 <50709196+RoadToNowhere2@users.noreply.github.com>
2019-11-25 19:55:22 +0400

    Add files via upload

--- ремонт/js/main.js
+++ ремонт/js/main.js
@@ -37,4 +37,124 @@ $(document).ready(function(){
         $('body', 'html').css('overflow', 'visible');
     });
     
+    {
+        $('.kviz_btn_1').on('click', function () {
+            $('.js-kviz-1').css("transform", "translateX(-100%)");
+            $('.js-kviz-1').css("opacity", "0");
+            $('.js-kviz-1').css("visibility", "hidden");
+
+            $('.js-kviz-2').css("transform", "translateX(0)");
+            $('.js-kviz-2').css("opacity", "1");
+            $('.js-kviz-2').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_2').on('click', function () {
+            $('.js-kviz-2').css("transform", "translateX(-100%)");
+            $('.js-kviz-2').css("opacity", "0");
+            $('.js-kviz-2').css("visibility", "hidden");
+
+            $('.js-kviz-3').css("transform", "translateX(0)");
+            $('.js-kviz-3').css("opacity", "1");
+            $('.js-kviz-3').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_2_prew').on('click', function () {
+            $('.js-kviz-2').css("transform", "translateX(100%)");
+            $('.js-kviz-2').css("opacity", "0");
+            $('.js-kviz-2').css("visibility", "hidden");
+
+            $('.js-kviz-1').css("transform", "translateX(0)");
+            $('.js-kviz-1').css("opacity", "1");
+            $('.js-kviz-1').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_3').on('click', function () {
+            $('.js-kviz-3').css("transform", "translateX(-100%)");
+            $('.js-kviz-3').css("opacity", "0");
+            $('.js-kviz-3').css("visibility", "hidden");
+
+            $('.js-kviz-4').css("transform", "translateX(0)");
+            $('.js-kviz-4').css("opacity", "1");
+            $('.js-kviz-4').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_3_prew').on('click', function () {
+            $('.js-kviz-3').css("transform", "translateX(100%)");
+            $('.js-kviz-3').css("opacity", "0");
+            $('.js-kviz-3').css("visibility", "hidden");
+
+            $('.js-kviz-2').css("transform", "translateX(0)");
+            $('.js-kviz-2').css("opacity", "1");
+            $('.js-kviz-2').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_4').on('click', function () {
+            $('.js-kviz-4').css("transform", "translateX(-100%)");
+            $('.js-kviz-4').css("opacity", "0");
+            $('.js-kviz-4').css("visibility", "hidden");
+
+            $('.js-kviz-5').css("transform", "translateX(0)");
+            $('.js-kviz-5').css("opacity", "1");
+            $('.js-kviz-5').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_4_prew').on('click', function () {
+            $('.js-kviz-4').css("transform", "translateX(100%)");
+            $('.js-kviz-4').css("opacity", "0");
+            $('.js-kviz-4').css("visibility", "hidden");
+
+            $('.js-kviz-3').css("transform", "translateX(0)");
+            $('.js-kviz-3').css("opacity", "1");
+            $('.js-kviz-3').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_5').on('click', function () {
+            $('.js-kviz-5').css("transform", "translateX(-100%)");
+            $('.js-kviz-5').css("opacity", "0");
+            $('.js-kviz-5').css("visibility", "hidden");
+
+            $('.js-kviz-6').css("transform", "translateX(0)");
+            $('.js-kviz-6').css("opacity", "1");
+            $('.js-kviz-6').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_5_prew').on('click', function () {
+            $('.js-kviz-5').css("transform", "translateX(100%)");
+            $('.js-kviz-5').css("opacity", "0");
+            $('.js-kviz-5').css("visibility", "hidden");
+
+            $('.js-kviz-4').css("transform", "translateX(0)");
+            $('.js-kviz-4').css("opacity", "1");
+            $('.js-kviz-4').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_6').on('click', function () {
+            $('.js-kviz-6').css("transform", "translateX(-100%)");
+            $('.js-kviz-6').css("opacity", "0");
+            $('.js-kviz-6').css("visibility", "hidden");
+
+            $('.js-kviz-7').css("transform", "translateX(0)");
+            $('.js-kviz-7').css("opacity", "1");
+            $('.js-kviz-7').css("visibility", "visible");
+        });
+
+        $('.kviz_btn_6_prew').on('click', function () {
+            $('.js-kviz-6').css("transform", "translateX(100%)");
+            $('.js-kviz-6').css("opacity", "0");
+            $('.js-kviz-6').css("visibility", "hidden");
+
+            $('.js-kviz-5').css("transform", "translateX(0)");
+            $('.js-kviz-5').css("opacity", "1");
+            $('.js-kviz-5').css("visibility", "visible");
+        });
+
+        $('.kviz_open').on('click', function() {
+            $('.modal_kviz').fadeIn(500);
+            $('.modal_kviz').css('display', 'flex');
+        });
+    }
+
+
+
+
 });

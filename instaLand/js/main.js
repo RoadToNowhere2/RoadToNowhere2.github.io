c0c17ecc4cc5008e6a74429ca44ce47c6d6db334
RoadToNowhere2 <50709196+RoadToNowhere2@users.noreply.github.com>
2020-05-24 14:35:46 +0400

    Update main.js

--- instaLand/js/main.js
+++ instaLand/js/main.js
@@ -8,11 +8,11 @@ $(document).ready(function() {
     });
     
     {
-        let offset  = $('#scroll-item').offset().top;
-        let doc_w   = $(document).width();
-    
-        console.log(offset)
         $('.js-scroll').on('click', function() {
+             let offset  = $('#scroll-item').offset().top;
+             let doc_w   = $(document).width();
+    
+             console.log(offset)
             $('body,html').animate({scrollTop: offset}, 1500);
             if (doc_w > 500) {
                 $('.desctope-container').animate({scrollTop: offset}, 1500);

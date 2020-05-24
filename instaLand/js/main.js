c3a8737f7a80b8a37a7f098be9b301bedcca9442
RoadToNowhere2 <50709196+RoadToNowhere2@users.noreply.github.com>
2020-05-24 12:27:00 +0300

    Add files via upload

--- instaLand/js/main.js
+++ instaLand/js/main.js
@@ -1,7 +1,18 @@
-$('.slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    arrows: false
-});
+$(document).ready(function() {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        arrows: false
+    });
+    
+    {
+        let offset = $('.packages').offset().top;
+    
+        console.log(offset)
+        $('.js-scroll').on('click', function() {
+            $('body,html').animate({scrollTop: offset}, 1500);
+        });
+    }
+})

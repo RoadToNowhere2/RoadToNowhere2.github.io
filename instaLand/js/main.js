0a7269c341081a669925a5189d91fb7d832a5d37
RoadToNowhere2 <50709196+RoadToNowhere2@users.noreply.github.com>
2020-05-24 21:52:16 +0300

    Add files via upload

--- instaLand/js/main.js
+++ instaLand/js/main.js
@@ -8,14 +8,41 @@ $(document).ready(function() {
     });
     
     {
-        $('.js-scroll').on('click', function() {
-             let offset  = $('#scroll-item').offset().top;
-             let doc_w   = $(document).width();
-    
-             console.log(offset)
-            $('body,html').animate({scrollTop: offset}, 1500);
-            if (doc_w > 500) {
-                $('.desctope-container').animate({scrollTop: offset}, 1500);
+        let button      = $('.js-scroll');
+
+        button.on('click', function() {
+            if ($(this).children('img').attr('src') == 'img/button-1.png') {
+                let offset  = $('#scroll-item').offset().top;
+                let doc_w   = $(document).width();
+
+                $('.button-animate').css('right', '-65px');
+
+                setTimeout(()=> {
+                    $('.button-animate').css('right', 'calc( 100% + 35px )');
+                }, 1000)
+            
+                console.log(offset)
+
+                $('body,html').animate({scrollTop: offset}, 1500);
+                if (doc_w > 500) {
+                    $('.desctope-container').animate({scrollTop: offset}, 1500);
+                }
+            } else if ($(this).children('img').attr('src') == 'img/button-2.png' || $(this).children('img').attr('src') == 'img/button-4.png') {
+                $(this).children('img.button-fade').fadeIn('fast')
+
+
+                $('.js-scroll img').attr('src') == 'img/button-1.png';
+                let offset  = $('#scroll-item').offset().top;
+                let doc_w   = $(document).width();
+            
+                console.log(offset)
+
+                $('body,html').animate({scrollTop: offset}, 1500);
+                if (doc_w > 500) {
+                    $('.desctope-container').animate({scrollTop: offset}, 1500);
+                }
+
+                $(this).children('img.button-fade').fadeOut(2000)
             }
         });
     }

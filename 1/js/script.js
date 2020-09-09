b801c3faa912d8675d2ecd80fa870010a895c7eb
RoadToNowhere2 <50709196+RoadToNowhere2@users.noreply.github.com>
2020-09-09 12:13:57 +0400

    Add files via upload

--- 1/js/script.js
+++ 1/js/script.js
@@ -2,12 +2,22 @@ $(document).ready(function () {
     $('.btn').on('click', () => {
         $('.modal-podarok').css('left', '0');
         $('.container').css('filter', 'blur(20px)');
+        if ($(window).width() <= '1000'){
+            $('body').css('overflow', 'hidden');
+            $('html').css('overflow', 'hidden');
+        }
     });
 
     $('.modal-podarok .close-modal').on('click', () => {
         $('.modal-podarok').css('left', '-100%');
         $('.container').css('filter', 'blur(0)');
+        if ($(window).width() <= '1000'){
+            $('body').css('overflow', 'scroll');
+            $('html').css('overflow', 'scroll');
+        }
     });
 
+    
 
+    
 });

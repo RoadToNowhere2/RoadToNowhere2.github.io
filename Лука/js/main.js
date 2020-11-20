41afa7f0630b1a7806e6ec3181846df176b52652
RoadToNowhere2 <50709196+RoadToNowhere2@users.noreply.github.com>
2020-11-20 14:41:11 +0400

    Add files via upload

--- Лука/js/main.js
+++ Лука/js/main.js
@@ -28,6 +28,37 @@ $(document).ready(function () {
           $('.header-menu').toggleClass('active');
         })
       
+        $('.video-inner__block .play').on('click', function () {
+          $('.modal').css('display', 'flex');
+        });
+
+        $('.modal-video__close').on('click', function () {
+          $('.modal').css('display', 'none');
+        });
       
       }
+
+      // {
+
+      //   var show = true;
+      //   var countbox = ".benefits__inner";
+      //   $(window).on("scroll load resize", function () {
+      //       if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      //       var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+      //       var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+      //       var w_height = $(window).height(); // Высота окна браузера
+      //       var d_height = $(document).height(); // Высота всего документа
+      //       var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+      //       if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+      //           $('.benefits__number').css('opacity', '1');
+      //           $('.benefits__number').spincrement({
+      //               thousandSeparator: "",
+      //               duration: 1200
+      //           });
+                
+      //           show = false;
+      //       }
+      //   });
+
+      // }
 });
